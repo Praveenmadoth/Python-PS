332646e0dc2a429cad0967e208c20df624a58848
praveenmadoth <madothpraveen@gmail.com>
2025-09-19 00:22:02 +0530

    loops

---
+++ PS4loops.py
@@ -0,0 +1,112 @@
+# 1. Print Numbers from 1 to n
+# n=int(input("enter the number:"))
+# i=1
+# while i<=n:
+#     print(i)
+#     i+=1
+
+#   2. Print num from n to 1 in Reverse
+# n=int(input("enter the number:"))
+# i=1
+# while i<=n:
+#     print(n)
+#     n-=1
+
+# 3. Print Numbers from m to n using loop
+# m=int(input("enter the m value :"))
+# n=int(input("enter the n value:"))
+# while m<n:
+#     print(m)
+#     m+=1
+
+# 4. Print Numbers from n to m in Reverse
+# n=int(input("enter the n value:"))
+# m=int(input("enter the m value :"))
+# while n>=m:
+#     print(n)
+#     n-=1
+
+# 5. Sum of n Natural Numbers
+# n=int(input("enter the n value:"))
+# i=0
+# sum=0
+# while i<=n:
+#     sum=sum+i
+#     i+=1
+# print(sum)    
+
+ 
+
+# 6.sum of m to n
+# n=int(input("enter the n value:"))
+# m=int(input("enter the m value :"))
+# sum=0
+# while m>=n:
+#     sum+=m
+#     m-=1
+# print("sum of the n and m is",sum) 
+
+# 7.Factorial of a Number 
+# n=int(input("enter the n value:"))
+# f=1
+# while n>=1:
+#     f=f*n
+#     n-=1
+# print("factorial of the given number is :,",f)  
+# 8.print even numbers from given range using while loop
+# m=int(input("enter the m value :"))
+# n=int(input("enter the n value:"))
+# while m<=n:
+#     if m%2==0:
+#         print(m)
+#     m+=1    
+# Print odd numbers in a given range using a while loop.
+# Example: Input → M = 1, N = 10
+# Output → 1,3,5,7,9
+# m=int(input("enter the m value :"))
+# n=int(input("enter the n value:"))
+# while m<=n:
+#     if m%2==1:
+#         print(m)
+#     m+=1    
+# 10.Count even and odd numbers in a given range using a while loop.
+# Example: Input → M = 1, N = 10
+# Output →
+# Even Count = 5     Odd Count = 5
+# m=int(input("enter the m value :"))
+# n=int(input("enter the n value:"))
+# oc=0
+# ec=0
+# while m<=n:
+#     if m%2==0:
+#         ec+=1
+#         m+=1
+#     else:
+#         oc+=1
+#         m+=1
+# print("even count",ec)  
+# print("odd count",oc)      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
