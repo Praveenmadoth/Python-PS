13511e4b1b06d9d8d93fd43480418e522c72eb8c
praveenmadoth <madothpraveen@gmail.com>
2025-09-20 19:11:53 +0530

    shallow&deepcopy

---
+++ deepandshallow.py
@@ -0,0 +1,10 @@
+# write a program on example of shallow copy and deepcopy.
+# shallow copy
+# import copy
+# l=[[2,4],[5,6]]
+# shallow=copy.copy (l)
+# deep=copy.deepcopy (l)
+# l[1][0]=96
+# print("orignal ",l)
+# print("shallow copy",shallow)
+# print("deep copy",deep)
